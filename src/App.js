40b9dbed316dd864dd79f15d24e7a55c0e2d36d0
Keith Billings <heyitsbo91@gmail.com>
2021-01-07 01:48:12 -0700

    improve comments, convert direction to useState and make it actually flip back and forth between A-Z and Z-A

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Jumbotron from "./components/Jumbotron";
 function App() {
   const [users, setUsers] = useState([]);
   const [filteredUsers, setFilteredUsers] = useState(users);
+  const [direction, setDirection] = useState("asc");
 
   useEffect(() => {
     API.getUsersData().then((userData) => {
@@ -26,26 +27,28 @@ function App() {
     let switchcount: number,
       i: number = 0;
 
-    // Set the sorting direction to ascending:
-    let direction: "asc" | "desc" = "asc";
+    // When a header is clicked it will initially sort A-Z
+    // This will flip it back and forth between A-Z and Z-A each click
+    if (direction === "asc") {
+      setDirection("desc");
+    } else {
+      setDirection("asc");
+    }
 
     // Make a loop that will continue until no switching has been done:
     while (switching) {
       let rows = table.rows;
       // Start by saying: no switching is done:
       switching = false;
-      /* Loop through all table rows (except the
-      first, which contains table headers): */
-      let lengthOfRowsMinusTheHeader = rows.length - 1; // declaring this a variable instead of putting it in the second paramter of the following for loop to avoid a bug that is generated when formatting document. The formatting removes the needed parenthesis
+      // Loop through all table rows (except the first, which contains table headers)
+      let lengthOfRowsMinusTheHeader = rows.length - 1; // declaring this a variable instead of putting it in the second paramter of the following for loop to avoid a bug that is generated when formatting document with the Prettier extension. The formatting removes the needed parenthesis
       for (i = 1; i < lengthOfRowsMinusTheHeader; i++) {
         // Start by saying there should be no switching:
         shouldSwitch = false;
-        /* Get the two elements you want to compare,
-        one from current row and one from the next: */
+        /* Get the two elements you want to compare, one from current row and one from the next: */
         let x = rows[i].getElementsByTagName("TD")[n];
         let y = rows[i + 1].getElementsByTagName("TD")[n];
-        /* Check if the two rows should switch place,
-        based on the direction, asc or desc: */
+        /* Check if the two rows should switch place, based on the direction, asc or desc: */
         if (direction === "asc") {
           if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
             // If so, mark as a switch and break the loop:
@@ -61,17 +64,14 @@ function App() {
         }
       }
       if (shouldSwitch) {
-        /* If a switch has been marked, make the switch
-        and mark that a switch has been done: */
+        /* If a switch has been marked, make the switch and mark that a switch has been done: */
         rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
         switching = true;
         // Each time a switch is done, increase this count by 1:
         switchcount++;
       } else {
-        /* If no switching has been done AND the direction is "asc",
-        set the direction to "desc" and run the while loop again. */
+        /* If no switching has been done, run the while loop again. */
         if (switchcount === 0 && direction === "asc") {
-          direction = "desc";
           switching = true;
         }
       }
